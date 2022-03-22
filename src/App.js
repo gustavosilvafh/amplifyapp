89e1327a6b2a7c306555e41d042035bb2bddc45f
Gustavo Silva <gustavofsilva@email.com>
2022-03-22 20:52:13 -0300

    update"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { withAuthenticator, Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-      </header>
-    </div>
+    <>
+      <Authenticator />; tests
+    </>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
